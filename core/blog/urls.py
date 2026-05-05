41cef870557da5df2e4d49c71e6ebe5bb99cd1a9
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-05-05 15:54:20 +0330

    added createview

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     #path('go-to-maktabkhooneh', views.RedirectToMaktab.as_view(), name='redirect-to-maktabkhooneh'),
     path('post/',views.PostListView.as_view(),name='post-list'),
     path('post/<int:pk>/',views.PostDetailView.as_view(),name='post-detail'),
+    path('post/create/',views.PostCreateView.as_view(),name="post-create"),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView, RedirectView
 from .models import Post
 from django.shortcuts import get_object_or_404
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,FormView,CreateView
+from .forms import PostForm
 # Create your views here.
 
 # Function Base View show a template
@@ -60,3 +61,23 @@ class PostDetailView(DetailView):
     """
     model = Post
     
+    
+'''
+class PostCreateView(FormView):
+    template_name = 'contact.html'
+    form_class = PostForm
+    success_url = '/blog/post/'
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+'''
+
+class PostCreateView(CreateView):
+    model = Post
+    # fields = ['author', 'title', 'content','status', 'category', 'published_date']
+    form_class = PostForm
+    success_url = '/blog/post/'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
