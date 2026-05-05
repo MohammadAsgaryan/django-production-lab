56f666644718e7038de951582bd21f207a9ddef1
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-05-05 10:40:03 +0330

    cbv first init and templates view

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def indexView(request):
+    name = "mmd"
+    context = {"name": name}
+    return render(request, "index.html", context)
