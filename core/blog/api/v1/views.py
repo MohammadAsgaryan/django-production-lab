e8b93d26761b8f3ba4719b5b95f0bfa57108e607
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-05-07 18:19:18 +0330

    404 and many
    + serializer many postlist
    + add 404

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,17 +2,20 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
-
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
 
 @api_view()
 def PostList(request):
-    return Response("ok")
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def postDetail(request, id):
-    post = Post.objects.get(pk=id)
+    post = get_object_or_404(Post, pk=id, status=True)
     serializer = PostSerializer(post)
     return Response(serializer.data)
