f35006b705f7f6b133ba1190d258d9fc9154a96d
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-05-07 19:06:09 +0330

    aadding post request to list

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -7,12 +7,17 @@ from rest_framework import status
 
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def PostList(request):
-    posts = Post.objects.filter(status=True)
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
-
+    if request.method == "GET":
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 @api_view()
 def postDetail(request, id):
