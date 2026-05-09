5d8d6b495ba2a47128c0b974143be44536f821c5
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-05-09 16:24:04 +0330

    serializer source fields

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -6,9 +6,11 @@ from ...models import Post, Category
 
 
 class PostSerializer(serializers.ModelSerializer):
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
     class Meta:
         model = Post
-        fields = ['id','author','title','content','category','status','created_date','published_date']
+        fields = ['id','author','title','content','snippet','category','status','relative_url','created_date','published_date']
         
         
         
