925ee7bf893e6a24cf6446736a694db2cfbcb75b
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-05-09 18:44:17 +0330

    adding ordering and searchfilter

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -12,6 +12,8 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter,OrderingFilter
+
 
 # Example for Function Based View
 '''
@@ -119,8 +121,10 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['category', 'author','status']
+    search_fields = ['title', 'content']
+    ordering_fields = ['published_date']
     
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
