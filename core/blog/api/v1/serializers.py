8b88f3383e82a7b50b34eaffa441dc0039abd797
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-05-09 14:46:45 +0330

    modelviewset for category and post

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ...models import Post
+from ...models import Post, Category
 # class PostSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=200)
@@ -9,3 +9,10 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id','author','title','content','category','status','created_date','published_date']
+        
+        
+        
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path, include
 from . import views
-
+from rest_framework.routers import DefaultRouter
 
 app_name = "api-v1"
 
-urlpatterns = [
-    #path('post/',PostList,name='Post-List'),
-    #path('post/',views.PostList.as_view(),name='Post-List'),
-    #path('post/<int:pk>/',views.PostDetail.as_view(),name="post-detail"),
-    path('post/',views.PostViewSet.as_view({'get':'list','post':'create'}),name='post-list'),
-    path('post/<int:pk>/',views.PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}),name="post-detail"),
- ]
+router = DefaultRouter()
+router.register('post',views.PostModelViewSet,basename='post')
+router.register('category',views.CategoryModelViewSet,basename='category')
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     #path('post/',PostList,name='Post-List'),
+#     #path('post/',views.PostList.as_view(),name='Post-List'),
+#     #path('post/<int:pk>/',views.PostDetail.as_view(),name="post-detail"),
+#     path('post/',views.PostViewSet.as_view({'get':'list','post':'create'}),name='post-list'),
+#     path('post/<int:pk>/',views.PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}),name="post-detail"),
+#  ]
