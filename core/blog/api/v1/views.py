0496e7460482b294b3219250b13d4e1e297bd33d
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-05-09 18:23:12 +0330

    adding object permission

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -10,6 +10,8 @@ from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from .permissions import IsOwnerOrReadOnly
+
 
 # Example for Function Based View
 '''
@@ -114,7 +116,7 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
     
 # Example for ViewSet in CBV
 class PostModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
 
