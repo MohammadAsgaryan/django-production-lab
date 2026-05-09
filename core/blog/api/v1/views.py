8e590aa2ba73ef4fd420e2fb002508c170951ada
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-05-09 19:10:14 +0330

    adding custom pagination class

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -13,7 +13,7 @@ from rest_framework.decorators import action
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter,OrderingFilter
-
+from .paginations import DefaultPagination
 
 # Example for Function Based View
 '''
@@ -125,7 +125,7 @@ class PostModelViewSet(viewsets.ModelViewSet):
     filterset_fields = ['category', 'author','status']
     search_fields = ['title', 'content']
     ordering_fields = ['published_date']
-    
+    pagination_class = DefaultPagination
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
