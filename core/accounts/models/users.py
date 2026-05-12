a6622c3fc6d9e867c16f86b5b6535aa01f9c4cb2
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-05-12 12:24:46 +0330

    adding is verified field

--- core/accounts/models/users.py
+++ core/accounts/models/users.py
@@ -2,10 +2,8 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin
 )
-from django.db.models.fields.related import ForeignKey
 from django.utils.translation import ugettext_lazy as _
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
 
 # Create your models here.
 
@@ -34,6 +32,8 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_verified', True)
+        
         
         
         if extra_fields.get('is_superuser') is not True:
@@ -51,7 +51,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    # is_verified = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -64,27 +64,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-    
-class Profile(models.Model):
-    """
-    Profile class for each user which is being created to hold the information
-    """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    image = models.ImageField(blank=True, null=True)
-    description = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.user.email
-    
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, created, **kwargs):
-    '''
-    signal for post creating user which activates when a user being created only
-    '''
-    
-    if created:
-        Profile.objects.create(user=instance)
